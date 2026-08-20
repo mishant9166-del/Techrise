2b12e5f766e8d4f97ee4038eaf4efe100dc263d5
VikashSinghrookie <vikaskuntalcool31@gmail.com>
2026-08-20 18:54:51 +0530

    Fix Qt6 TLS plugin deployment for Windows and macOS

--- cmake/common/helpers_common.cmake
+++ cmake/common/helpers_common.cmake
@@ -235,6 +235,7 @@ function(find_qt_plugins)
     iconengines
   )
   list(APPEND qt_plugins_Gui platforminputcontexts)
+  list(APPEND qt_plugins_Network tls)
   list(APPEND qt_plugins_Sql sqldrivers)
   list(APPEND qt_plugins_3dRender sceneparsers geometryloaders)
   list(APPEND qt_plugins_3dQuickRender renderplugins)
